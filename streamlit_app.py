719d8b66a8949fe362c6134f04463234a0f54375
socheataly <73855820+socheataly@users.noreply.github.com>
2024-06-20 10:50:17 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 09:38:41 2024
+
+@author: sokip
+"""
+
+from Bio import Restriction
+# print (Restriction.BspQI)
+# print (Restriction.BspQI.site)
+
+print ("%s | %s" % (Restriction.BspQI, Restriction.BspQI.site))
